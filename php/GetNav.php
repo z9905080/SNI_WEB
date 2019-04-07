0e895951bd2f34db035fd8eb9f6153e13f03bd6d
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-04-07 22:25:50 +0800

    test

--- php/GetNav.php
+++ php/GetNav.php
@@ -7,4 +7,26 @@ $db = new MysqliDb($dbCofig);
 $data = $db->join("page_group pg", "pg.id=pc.page_group_id", "INNER")
     ->get('page_content pc', null, "pc.id as page_content_id, page_group_id, group_name, page_name");
 
-echo (json_encode($data));
+$dataRlt = array();
+
+foreach ($data as $key => $originItem) {
+    foreach ($dataRlt as $key => $newItem) {
+        if ($newItem["page_group_id"] != $originItem["page_group_id"]) {
+            array_push($dataRlt, array(
+                "page_group_id" => $originItem["page_group_id"],
+                "group_name" => $originItem["group_name"],
+                "page_content" => array(
+                    "page_content_id" => $originItem["page_content_id"],
+                    "page_name" => $originItem["page_name"],
+                ),
+            ));
+        } else {
+            array_push($newItem["page_content"],array(
+                "page_content_id" => $originItem["page_content_id"],
+                "page_name" => $originItem["page_name"],
+            ));
+        }
+    }
+}
+
+echo (json_encode($dataRlt));

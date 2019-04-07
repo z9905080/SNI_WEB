14a213faf571ec1b277f49338a003936066c8e67
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-04-07 21:15:20 +0800

    修正 ㄨㄣˋ

--- php/main.php
+++ php/main.php
@@ -1,5 +1,11 @@
 <?php
 
+
+// Cross-Origin Resource Sharing Header
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+
 require_once 'MysqliDb.php';
 
 $db = new MysqliDb(array(

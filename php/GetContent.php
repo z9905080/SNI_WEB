7a676f9b56de0b7601f1a199936f31b33ce4807f
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-04-14 16:04:47 +0800

    [update]改格式

--- php/GetContent.php
+++ php/GetContent.php
@@ -1,13 +1,17 @@
 <?php
 
-
 if ( $_SERVER['REQUEST_METHOD'] !== 'POST' )
 {
     // http_response_code(404);
     // exit;
 }
 
-$pageID = $_REQUEST["page_id"];
+
+$data = json_decode(file_get_contents('php://input'), true);
+echo $data;
+
+$pageID = $data->page_id;
+//$pageID = $_REQUEST["page_id"];
 
 if ($pageID == "") {
     http_response_code(404);

8e3b6732963273bb6885b6f69b1023e078833900
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-04-20 11:57:31 +0800

    [update] Laravel版本

--- php/setting.php
+++ php/setting.php
@@ -1,6 +1,8 @@
 <?php
+use function Symfony\Component\Debug\header;
 
 // Cross-Origin Resource Sharing Header
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
+header('Cache-Controll: max-age=3600');

7c1362c520cd871e1dac90a282b5c7d392c406c0
hulk <gash60535@gmail.com>
2019-05-12 07:05:37 +0800

    [fix][admin]upload

--- node-vue-ele-app/client/src/views/Edit.vue
+++ node-vue-ele-app/client/src/views/Edit.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
     <div class="form">
-      <el-form
-        ref="form"
-        label-width="120px"
-        style="margin:10px;width:auto;"
-      >
+      <el-form ref="form" label-width="120px" style="margin:10px;width:auto;">
+        <el-upload
+          id="upload"
+          ref="upload"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :on-preview="handlePreview"
+          :on-success="insertImage"
+          :on-remove="handleRemove"
+          :before-remove="beforeRemove"
+          multiple
+          :on-exceed="handleExceed"
+          :file-list="fileList"
+        >
+          <el-button ref="imageBtn" size="small" type="primary">点击上传</el-button>
+        </el-upload>
         <!--
     给editor加key是因为给tinymce keep-alive以后组件切换时tinymce编辑器会显示异常，
     在activated钩子里改变key的值可以让编辑器重新创建
         -->
         <editor id="tinymceEditor" :init="tinymceInit" v-model="content" :key="tinymceFlag"></editor>
+
         <el-form-item class="text_right">
           <el-button type="primary" @click="onSubmit()">提交</el-button>
           <el-button @click="onCancel()">取消</el-button>
@@ -41,7 +52,8 @@ export default {
     return {
       tinymceFlag: 1,
       tinymceInit: {},
-      content: ""
+      content: "",
+      fileList: []
     };
   },
   methods: {
@@ -61,17 +73,36 @@ export default {
               type: "success"
             });
             this.$emit("update");
-            this.$router.push('/articlelist');
+            this.$router.push("/articlelist");
           });
         }
       });
     },
     onCancel() {
-      this.$router.push('/articlelist');
+      this.$router.push("/articlelist");
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${
+          files.length
+        } 个文件，共选择了 ${files.length + fileList.length} 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    },
+    test() {
+      let imageBtn = this.$refs.imageBtn;
+      console.log(imageBtn);
     }
   },
   created() {
-    const that = this;
     this.tinymceInit = {
       skin_url: "/tinymce/skins/ui/oxide",
       language_url: `/tinymce/langs/zh_CN.js`,
@@ -86,18 +117,28 @@ export default {
       plugins: "advlist table lists paste preview fullscreen",
       toolbar:
         "fontselect fontsizeselect forecolor backcolor bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | imageUpload quicklink h2 h3 blockquote table numlist bullist preview fullscreen",
+      file_browser_callback: function(field_name, url, type, win) {
+        win.document.getElementById(field_name).value = "my browser value";
+      },
+      file_browser_callback_types: "file image media"
       /**
        * 下面方法是为tinymce添加自定义插入图片按钮
        * 借助iview的Upload组件,将图片先上传到存储云上，再将图片的存储地址放入编辑器内容
        */
-      setup: editor => {
-        editor.ui.registry.addButton("imageUpload", {
-          // text: '插入图片',
-          tooltip: "插入图片",
-          icon: "image",
-          onAction: () => {}
-        });
-      }
+      // setup: editor => {
+      //   editor.ui.registry.addButton("imageUpload", {
+      //     // text: '插入图片',
+      //     tooltip: "插入图片",
+      //     icon: "image",
+      //     onAction: () => {
+      //       // let upload = this.$refs.upload;
+      //       // console.log(upload);
+      //       let imageBtn = this.$refs.imageBtn;
+      //       console.log(imageBtn);
+      //       imageBtn.handleClick();
+      //     }
+      //   });
+      // }
     };
   },
   activated() {

7e3af3b065f5ee8d3acb70f1690300cc1623f4bd
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-06-01 18:41:28 +0800

    [update] 增加刪除頁籤/內文api

--- php/GetNav.php
+++ php/GetNav.php
@@ -4,7 +4,7 @@ require 'main.php';
 
 $db = new MysqliDb($dbCofig);
 
-$data = $db->join("page_content pc", "pg.id=pc.page_group_id", "INNER")
+$data = $db->join("page_content pc", "pg.id=pc.page_group_id", "LEFT")
     ->get('page_group pg', null, "pc.id as page_content_id, page_group_id, group_name, page_name");
 
 $dataRlt = array();

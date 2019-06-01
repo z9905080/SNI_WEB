c726bd0cc2e83ef1df819f196bd699db32558d87
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2019-06-01 19:05:16 +0800

    [update] 增加刪除頁籤/內文api

--- php/GetNav.php
+++ php/GetNav.php
@@ -31,31 +31,4 @@ foreach ($pageGroupList as $index1 => $pageGroupData) {
     $dataRlt[] = $newGroupData;
 }
 
-// foreach ($data as $key => $originItem) {
-//     $isExist = false;
-//     foreach ($dataRlt as $key => $newItem) {
-//         if ($newItem["page_group_id"] == $originItem["page_group_id"]) {
-//             array_push($dataRlt[$key]["page_content"], array(
-//                 "page_content_id" => $originItem["page_content_id"],
-//                 "page_name" => $originItem["page_name"],
-//             ));
-//             $isExist = true;
-//         }
-//     }
-
-//     if (!$isExist) {
-//         array_push($dataRlt, array(
-//             "page_group_id" => $originItem["page_group_id"],
-//             "group_name" => $originItem["group_name"],
-//             "page_content" => array(
-//                 array(
-//                     "page_content_id" => $originItem["page_content_id"],
-//                     "page_name" => $originItem["page_name"],
-//                 ),
-//             ),
-//         ));
-//     }
-
-// }
-
 echo (json_encode($dataRlt));

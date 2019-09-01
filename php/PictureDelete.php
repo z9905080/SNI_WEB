222ae2e5939241a272685a9341cfa6404d67f01d
Shouting <z9905080@gmail.com>
2019-09-01 14:38:10 +0800

    [update] 修正圖片刪除錯誤

--- php/PictureDelete.php
+++ php/PictureDelete.php
@@ -9,36 +9,22 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $filePath = $data['file_path'];
 
-if ($filePath == ""){
+if ($filePath == "") {
     http_response_code(404);
     exit;
 }
 
-if(file_exists($filePath)){
-        //將檔案刪除
-    if (unlink($filePath)){
-        $resp = array(
-            "data" => null,
-            "message" => "刪除圖片成功",
-            "code" => Code::DeletePicture_Success,
-            "status" => "Y",
-        );
-        
-        echo (json_encode($resp));
-    }else{
-        $resp = array(
-            "data" => null,
-            "message" => "刪除圖片失敗",
-            "code" => Code::DeleteContent_Fail,
-            "status" => "N",
-        );
+$resp = array();
+
+if (file_exists($filePath)) {
+    //將檔案刪除
+    if (unlink($filePath)) {
+        $resp = (new APIResponse(null, "刪除圖片成功", Code::DeletePicture_Success, "Y"))->GetAPIResponse();
+    } else {
+        $resp = (new APIResponse(null, "刪除圖片失敗", Code::DeleteContent_Fail, "N"))->GetAPIResponse();
     }
-    
-}else{
-    $resp = array(
-        "data" => null,
-        "message" => "刪除圖片失敗,檔案不存在",
-        "code" => Code::DeleteContent_Fail,
-        "status" => "N",
-    );
+} else {
+    $resp = (new APIResponse(null, "刪除圖片失敗,檔案不存在", Code::DeleteContent_Fail, "N"))->GetAPIResponse();
 }
+
+echo (json_encode($resp));

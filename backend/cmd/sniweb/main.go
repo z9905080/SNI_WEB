a44bc9b710fe0fbe722de748a535c4763f5b605b
shouting.hsiao <shouting.hsiao@gmail.com>
2026-09-17 07:22:05 +0800

    [chore] 舊程式碼移至 legacy/，建立 Go module 骨架

---
+++ backend/cmd/sniweb/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stderr, "sniweb：尚未實作")
+	os.Exit(2)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/z9905080/SNI_WEB
+
+go 1.25.0
